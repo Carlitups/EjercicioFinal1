02028fe7351dc22b77c2c47ece50e58404eb3cd4
Carlitups@gmail.com <Carlitups@gmail.com>
2023-08-30 16:48:09 +0200

    Cambio realizado

--- src/com/java/clases/Main.java
+++ src/com/java/clases/Main.java
@@ -7,7 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner scanner = new Scanner(System.in);
+		Scanner scani = new Scanner(System.in);
 		int numero = 0;
 		do {
 			System.out.println("Menu: ");
